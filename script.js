8b55f1004857056c2a467a6a4ed193dea59853ec
TMGenius29 <vedvyassf@gmail.com>
2025-09-12 06:24:23 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const btt = document.querySelector('.back-to-top');
   if (!btt) return;
 
-  const TOGGLE_AT = 300; // px scrolled before showing the button
+  const TOGGLE_AT = 275; // px scrolled before showing the button
   function toggleBTT() {
     if (window.scrollY > TOGGLE_AT) btt.classList.remove('is-hidden');
     else btt.classList.add('is-hidden');
